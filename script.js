f0c33e847a1414c717e5432b5dc8aee65d14054c
nicodemoos <victor.nico1@hotmail.com.br>
2026-02-19 14:12:52 -0300

    Adicionado funções no botão de gerar e copiar

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
-const botaoSalvar = document.querySelector(".btn-acao-salvar");
-const icone = botaoSalvar.querySelector("i"); 
+// 1. Use querySelectorAll para permitir o uso do forEach
+const botoesGerar = document.querySelectorAll(".btn-acao-gerar");
+const botoesCopiar = document.querySelectorAll(".btn-acao-copiar");
+const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+const botoesSalvar = document.querySelectorAll(".btn-acao-salvar");
+const inputSenha = document.querySelector(".input-senha");
 
 
-botaoSalvar.forEach((botao) => {
-    botao.addEventListener("click", (evento) => {
-        // CORREÇÃO: Buscamos o ícone dentro do botão que recebeu o clique
-        const icone = botao.querySelector("i"); 
+function gerarSenha(tamanho) {
+    let senhaMontada = ""; 
+    for (let i = 0; i < tamanho; i++) {
+        const indice = Math.floor(Math.random() * caracteres.length);
+        senhaMontada += caracteres.charAt(indice);
+    }
+    return senhaMontada; 
+}
+
+function copiarSenha(textoCopiado) {
+    textoCopiado = navigator.clipboard.writeText(inputSenha.value);
+}
+
+botoesCopiar.forEach((botao) => {
+    botao.addEventListener("click", () => {
+        copiarSenha();
+        
+        // Biblioteca SweetAlert2 para exibir um alerta de sucesso
+    Swal.fire({
+        title: "Senha Copiada!",
+        icon: "success",
+        theme: "dark",
+        timerProgressBar: true,
+        timer: 1500
+        });
+
+    });
+});
+
+botoesGerar.forEach((botao) => {
+    botao.addEventListener("click", () => {
+        const senha = gerarSenha(12);
+        document.querySelector(".input-senha").value = senha;
+
+        return senha;
+    });
+});
+
+
+
+botoesSalvar.forEach((botao) => {
+    botao.addEventListener("click", () => {
+
+        const icone = botao.querySelector("i");
         
         if (icone.classList.contains("fa-regular")) {
             icone.classList.replace("fa-regular", "fa-solid");
         } else {
             icone.classList.replace("fa-solid", "fa-regular");
         }
+        
+        
     });
 });
